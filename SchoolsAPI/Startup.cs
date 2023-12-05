633ee769728461afd89fb7737229dd0cf883074e
Endi Octaviano <endi.octaviano@gmail.com>
2023-12-05 10:04:16 +0700

    penambahan swagger (masih failed to load swagger)

--- SchoolsAPI/Startup.cs
+++ SchoolsAPI/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using SchoolsAPI.Configuration;
 using SchoolsAPI.DataAccess;
 
@@ -47,14 +48,50 @@ Database Name   : {1}", appSettings.MainDB.Provider.ToString(), db.Database.GetD
                     x.SerializerSettings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
                 });
 
-            //services.AddSwaggerGen
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ANTAM_FI_AppSysV2.API", Version = "v1" });
+//                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+//                {
+//                    Description =
+//"JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+//                    Name = "Authorization",
+//                    In = ParameterLocation.Header,
+//                    Type = SecuritySchemeType.ApiKey,
+//                    Scheme = "Bearer"
+//                });
+//                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+//                {
+//                    {
+//                        new OpenApiSecurityScheme
+//                        {
+//                            Reference = new OpenApiReference
+//                            {
+//                                Type = ReferenceType.SecurityScheme,
+//                                Id = "Bearer"
+//                            },
+//                            Scheme = "oauth2",
+//                            Name = "Bearer",
+//                            In = ParameterLocation.Header,
+
+//                        },
+//                        new List<string>()
+//                    }
+//                });
+
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseDeveloperExceptionPage();
-            //app.UseSw
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "SchoolsAPI");
+                c.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.None);
+            }); ;
             app.UseRouting();
             app.UseAuthentication();
             app.UseAuthorization();
